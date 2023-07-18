639aea996570b0667d154d1256e9220085f92bda
karim <limsu_@naver.com>
2023-07-18 15:07:04 +0900

    [ 섹션3. 서블릿, JSP, MVC 패턴 완료 ]
    BREAKING CHANGE: 2023-07-18

---
+++ servlet/src/main/java/hello/servlet/web/servletmvc/mvcMemberSaveServlet.java
@@ -0,0 +1,35 @@
+package hello.servlet.web.servletmvc;
+
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "mvcMemberSaveServlet", urlPatterns = "/servlet-mvc/members/save")
+public class mvcMemberSaveServlet extends HttpServlet {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String username = req.getParameter("username");
+        int age = Integer.parseInt(req.getParameter("age"));
+
+        Member member = new Member(username, age);
+
+        memberRepository.save(member);
+
+        // model에 데이터를 보관
+        req.setAttribute("member", member);
+
+        String viewPath = "/WEB-INF/views/save.jsp";
+        DispatcherType dispatcherType;
+    }
+}
---
+++ servlet/src/main/webapp/jsp/members/save.jsp
@@ -0,0 +1,16 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+성공
+<ul>
+    <li>id=<%member.getId()%></li>
+    <li>username=<%member.getUsername()%></li>
+    <li>age=<%member.getAge()%></li>
+</ul>
+<a href="/index.htlm">main</a>
+</body>
+</html>
+
