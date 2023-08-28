60cbcf472c4c8b82f3707a5738d5d008a006372a
sblim <sblim@netand.co.kr>
2023-08-28 10:00:56 +0900

    [ 스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술 - MVC 프레임워크 만들기 ] - 완료
    BREAKING CHANGE: 2023-08-28

---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v4/controller/MemberListControllerV4.java
@@ -0,0 +1,33 @@
+package hello.servlet.web.frontcontroller.v4.controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+import hello.servlet.web.frontcontroller.v4.ControllerV4;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author : sblim
+ * @version : 1.0.0
+ * @package : hello.servlet.web.frontcontroller.v1.controller
+ * @name : spring-basic-server
+ * @date : 2023. 08. 02. 002 오후 1:35
+ * @modifyed :
+ * @description :
+ **/
+public class MemberListControllerV4 implements ControllerV4 {
+
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+
+        List<Member> members = memberRepository.findAll();
+
+        model.put("members", members);
+        return "members";
+    }
+}
---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v4/controller/MemberSaveControllerV4.java
@@ -0,0 +1,37 @@
+
+package hello.servlet.web.frontcontroller.v4.controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+import hello.servlet.web.frontcontroller.v4.ControllerV4;
+
+import java.util.Map;
+
+/**
+ * @author : sblim
+ * @version : 1.0.0
+ * @package : hello.servlet.web.frontcontroller.v1.controller
+ * @name : spring-basic-server
+ * @date : 2023. 08. 02. 002 오후 1:35
+ * @modifyed :
+ * @description :
+ **/
+public class MemberSaveControllerV4 implements ControllerV4 {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+
+        String username = paramMap.get("username");
+        int age = Integer.parseInt(paramMap.get("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        model.put("member", member);
+        return "save-result";
+    }
+}
