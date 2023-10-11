18e121467586074c2390e4a08d35c3719b548418
sblim <sblim@netand.co.kr>
2023-10-11 11:27:07 +0900

    [ 백엔드개발자 위한 프론트엔드 기본 by foo init ] - 프로젝트 틀 설정
    BREAKING CHANGE: 2023-10-11

---
+++ frontend4backendbasic/src/main/java/com/example/frontend4backendbasic/Frontend4backendbasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.frontend4backendbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Frontend4backendbasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Frontend4backendbasicApplication.class, args);
+	}
+
+}
---
+++ frontend4backendbasic/src/test/java/com/example/frontend4backendbasic/Frontend4backendbasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.frontend4backendbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Frontend4backendbasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
