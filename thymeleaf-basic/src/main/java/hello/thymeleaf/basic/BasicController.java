d8de6ba0c6e857977ac635fe339dea88b0009ed1
Karim <limsu_@naver.com>
2024-01-08 14:30:51 +0900

    [ 스프링 MVC 2편 - 타임리프(기본 객체들) ]
    BREAKING CHANGE: 2024-01-08

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,9 +1,19 @@
 package hello.thymeleaf.basic;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author : sblim
@@ -34,5 +44,54 @@ public class BasicController {
     public String textUnescaped(Model model) {
         model.addAttribute("data", "<b>Hello Spring</b>");
         return "basic/text-unescaped";
+    }
+
+    @GetMapping("/variable")
+    public String variable(Model model) {
+
+        User userA = new User("a", 1);
+        User userB = new User("b", 2);
+
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        Map<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+
+        return "basic/variable";
+    }
+
+    @GetMapping("/basic-objects")
+    public String basicObjects(Model model, HttpServletRequest request,
+                               HttpServletResponse response, HttpSession session) {
+    session.setAttribute("sessionData", "Hello Session");
+    model.addAttribute("request", request);
+    model.addAttribute("response", response);
+    model.addAttribute("servletContext", request.getServletContext());
+    return "basic/basic-objects";
+    }
+
+    @Component("helloBean")
+    static class HelloBean{
+        public String hello(String data) {
+            return "Hello" + data;
+        }
+    }
+
+    @Data
+    static public class User {
+        private String username;
+        private int age;
+
+        public User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
     }
 }
